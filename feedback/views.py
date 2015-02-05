1a3230fb8781d0548b46c007cf0096366ce209a2
Richard Shea <rshea@thecubagroup.com>
2015-02-05 18:46:07 +1300

    Some changes I made to get django-feedback to work the way I wanted it to for a specific project. Before this repos gets forked or pushed some thought should be given to how generally applicable these changes are.

--- feedback/views.py
+++ feedback/views.py
@@ -6,6 +6,14 @@ from feedback.forms import AnonymousFeedbackForm, FeedbackForm
 
 
 def leave_feedback(request, template_name='feedback/feedback_form.html'):
+    print "In leave feedback 3"
+    print request.method
+    print "In leave feedback 4"
+    if request.method == "GET":
+        nexturl = request.GET.get('next')
+    else:
+        nexturl = request.POST.get('next')
+
     if request.user.is_authenticated():
         form = FeedbackForm(request.POST or None)
     else:
@@ -19,7 +27,18 @@ def leave_feedback(request, template_name='feedback/feedback_form.html'):
         feedback.save()
         messages.add_message(request, messages.SUCCESS,
                              'Your feedback was submitted.')
-        return HttpResponseRedirect(request.POST.get('next',
-                                    request.META.get('HTTP_REFERER', '/')))
-    return render_to_response(template_name, {'feedback_form': form},
-                              context_instance=RequestContext(request))
+        print "In leave feedback 1"
+        print nexturl
+        print request.POST.get('next')
+        print request.GET.get('next')
+        print request.META.get('HTTP_REFERER', '/')
+        print (request.POST.get('next', request.META.get('HTTP_REFERER', '/')))
+        print "In leave feedback 2"
+        return HttpResponseRedirect(nexturl)
+    return render_to_response(
+                                template_name, 
+                                {
+                                'feedback_form': form,
+                                'next': nexturl
+                                },
+                                context_instance=RequestContext(request))
